cf51ee5284392ecf51795b2d556cb3cd4862ff14
Phúc HP <phuchp.613@gmail.com>
2021-05-17 11:33:28 +0700

    Update version 3.1 (Update many permission in a module)

--- modules/Base/Console/PermissionCommand.php
+++ modules/Base/Console/PermissionCommand.php
@@ -50,19 +50,12 @@ class PermissionCommand extends Command{
         DB::unprepared($statement);
         /** Insert permission list */
         foreach($permissions as $value){
-            $data  = [
-                'name'         => trim($value['name']),
-                'display_name' => ucfirst($value['display_name']),
-                'parent_id'    => 0,
-            ];
-            $group = Permission::firstOrCreate($data);
-            if(isset($value['group']) && count($value['group']) > 0){
-                foreach($value['group'] as $subs => $sub){
-                    Permission::firstOrCreate([
-                        'name'      => trim($sub['name']),
-                        'parent_id' => $group->id,
-                    ], $sub);
+            if(!isset($value['name'])){
+                foreach($value as $item){
+                    self::updatePermission($item);
                 }
+            }else{
+                self::updatePermission($value);
             }
         }
 
@@ -75,14 +68,14 @@ class PermissionCommand extends Command{
 
         /** Delete permission id not exist */
         $permission_role_other_list = $permission_role_others->where('role_id', '<>', $admin_role->id)
-            ->groupBy('permission_id')
-            ->get();
+                                                             ->groupBy('permission_id')
+                                                             ->get();
 
         foreach($permission_role_other_list as $value){
             if(empty($value->permission)){
                 $permission_role_others->where('permission_id', $value->permission_id)
-                    ->where('role_id', $value->role_id)
-                    ->delete();
+                                       ->where('role_id', $value->role_id)
+                                       ->delete();
             }
         }
 
@@ -96,5 +89,25 @@ class PermissionCommand extends Command{
         }
 
         $this->info('Update Permission Successfully');
+    }
+
+    /**
+     * @param $value
+     */
+    public static function updatePermission($value){
+        $data  = [
+            'name'         => trim($value['name']),
+            'display_name' => ucfirst($value['display_name']),
+            'parent_id'    => 0,
+        ];
+        $group = Permission::firstOrCreate($data);
+        if(isset($value['group']) && count($value['group']) > 0){
+            foreach($value['group'] as $subs => $sub){
+                Permission::firstOrCreate([
+                                              'name'      => trim($sub['name']),
+                                              'parent_id' => $group->id,
+                                          ], $sub);
+            }
+        }
     }
 }
